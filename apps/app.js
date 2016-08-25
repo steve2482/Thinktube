75081e2c89775794ac3eebb426715afdd33da9d0
steve2482 <stevecb12684@yahoo.com>
2016-08-25 12:35:23 -0400

    changed .each to for loop, page styled

--- apps/app.js
+++ apps/app.js
@@ -2,8 +2,8 @@ $(function() {
   $('#searchTerm').submit(function(e) {
     e.preventDefault();
     var userKeyword = $('#searchInput').val();
-    console.log(userKeyword);
     getRequest(userKeyword);
+    clearForm();
   });
 
   function getRequest(userKeyword) {
@@ -14,19 +14,29 @@ $(function() {
     };
     var url = 'https://www.googleapis.com/youtube/v3/search';
     $.getJSON(url, params, function(data) {
-      console.log(data);
       showResults(data);
     });
   }
 
   function showResults(results) {
-    var html = '';
-    var i = 0;
-    $.each(results, function(index, value) {
-      html += '<img class="thumbnail" src="' + results.items[0][i].snippet.thumbnails.medium.url + '">';
-      console.log(results.items[1].snippet.thumbnails.medium.url);
-      // i++;
-    });
-    $('.results').html(html);
+    $('.results').empty();
+    for (var i = 0; i < results.items.length; i++) {
+      var title = results.items[i].snippet.title;
+      var videoId = results.items[i].id.videoId;
+      var Content = '<a href="https://www.youtube.com/watch?v=' + videoId + '"><img class="thumbnail" src="' + results.items[i].snippet.thumbnails.medium.url + '"></a><br><p class="title"><a href="https://www.youtube.com/watch?v=' + videoId + '">' + title + '</a>';
+      $('.results').append(Content);
+    }
+
+    // ----------------This did not work------------------
+    // var html = '';
+    // var i = 0;
+    // $.each(results, function(index, value) {
+    //   html += '<img class="thumbnail" src="' + results.items[i].snippet.thumbnails.medium.url + '">';
+    // });
+    // $('.results').html(html);
+  }
+
+  function clearForm() {
+    document.getElementById('searchTerm').reset();
   }
 });
